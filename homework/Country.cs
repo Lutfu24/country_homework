3ea393c778d4f1364f3d2e69be94c925fd439334
Lutfu24 <lutfuhm@code.edu.az>
2022-12-19 16:29:34 +0400

    country

---
+++ homework/Country.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Country
+    {
+        public string Name;
+        City[] _arr;
+        static int result;
+        public Country(string name)
+        {
+            Name = name;
+            _arr = new City[0];
+        }
+
+
+        public int TotalPopulation(City _city)
+        {
+            Array.Resize(ref _arr, _arr.Length + 1);
+            _arr[_arr.Length - 1] = _city;
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                result += _city.Population;
+                Console.Clear();
+                return result;
+            }
+            return result;
+        }
+
+        public void FindAllByPopulation(int min, int max)
+        {
+            foreach (City _city in _arr)
+            {
+                if (_city.Population<1000000 && _city.Population>300000)
+                {
+                    Console.WriteLine(_city);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country("Turkiye");
+            //Country country1 = new Country("Almaniya");
+            //Country country2 = new Country("America");
+            City city = new City("Ankara",400000);
+            City city1 = new City("Istanbul",450000);
+            City city2 = new City("Bursa", 480000);
+
+            //City city3 = new City("Berlin");
+            //City city4 = new City("Nyu-York");
+            //City city5 = new City("Las Vegas");
+            Console.WriteLine(country.TotalPopulation(city));
+            Console.WriteLine(country.TotalPopulation(city1));
+            Console.WriteLine(country.TotalPopulation(city2));
+            country.FindAllByPopulation(300000, 1000000);
+
+
+            //country.FindAllByPopulation(300000,1000000);
+
+
+        }
+    }
+}
